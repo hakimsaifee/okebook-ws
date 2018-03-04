b324675ed5d8a032c3a3221769bd9c409b55a039
Ajitesh.k <Ajitesh.k@BSL-BNG-L226.betsol.com>
2018-03-04 17:14:04 +0530

    Committed pom to bundle all jars together

--- ebook-parent/ebook-ui/src/main/java/com/ebook/EbookApplicationLoader.java
+++ ebook-parent/ebook-ui/src/main/java/com/ebook/EbookApplicationLoader.java
@@ -2,12 +2,14 @@ package com.ebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ebook.services.service.NotificationService;
 
 @SpringBootApplication(scanBasePackages = "com.ebook")
+@ImportResource("classpath:application-context-services.xml")
 @RestController
 public class EbookApplicationLoader {
 
