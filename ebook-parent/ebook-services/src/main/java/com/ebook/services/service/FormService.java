2964fed5f2962a26b2cdc6f921b9aa46ca53eeb0
Hakim Saifee <hakimsaifee08@gmail.com>
2018-06-19 23:55:14 +0530

    Added service for updation of form /schedule

--- ebook-parent/ebook-services/src/main/java/com/ebook/services/service/FormService.java
+++ ebook-parent/ebook-services/src/main/java/com/ebook/services/service/FormService.java
@@ -5,18 +5,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ebook.common.dto.FormDTO;
+import com.ebook.common.dto.PartDTO;
 import com.ebook.domain.entity.Form;
 import com.ebook.domain.repository.FormRepository;
 
 @Service
-public class FormService extends AbstractService<Form, FormDTO, FormRepository>{
+public class FormService extends AbstractService<Form, FormDTO, FormRepository> {
 
 	public FormService() {
 	}
-	
+
 	@Autowired
 	public FormService(FormRepository repository, DozerBeanMapper dozerBeanMapper) {
 		super(repository, dozerBeanMapper);
 	}
-	
+
+	@Override
+	public FormDTO update(FormDTO dto) {
+		Form savedForm = repository.findOne(dto.getId());
+		if (savedForm == null) {
+			return null;
+		}
+		if (savedForm.getPart() != null) {
+			PartDTO savedPart = beanMapper.map(savedForm.getPart(), PartDTO.class);
+			dto.setPart(savedPart);
+		}
+		return super.update(dto);
+	}
 }
